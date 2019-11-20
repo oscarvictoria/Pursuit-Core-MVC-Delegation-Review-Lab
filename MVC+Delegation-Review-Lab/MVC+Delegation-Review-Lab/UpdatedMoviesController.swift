fc2c83796740493b479165089cfd7f04f6c4e283
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-11-20 17:42:52 -0500

    updated the stepper to increment its value

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/UpdatedMoviesController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/UpdatedMoviesController.swift
@@ -10,13 +10,33 @@ import UIKit
 
 class UpdatedMoviesController: UIViewController {
 
+    @IBOutlet weak var updatedLabel: UILabel!
+    @IBOutlet weak var theSlider: UISlider!
+    @IBOutlet weak var theStepper: UIStepper!
+    
+    var font: Double = 17.0 {
+        didSet {
+            updatedLabel.text = "Font Size: \(theStepper.value)"
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+        configuredStepper()
     }
+    
+    func configuredStepper() {
+          theStepper.minimumValue = 1
+          theStepper.maximumValue = 45
+          theStepper.stepValue = 1
+          theStepper.value = 17
+      }
 
-
-
- 
+    @IBAction func theFunction(_ sender: UIStepper) {
+        font = sender.value
+    }
+    
+    
     
 }
